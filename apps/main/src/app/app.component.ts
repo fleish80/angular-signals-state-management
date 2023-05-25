947618d51ce68caa3e7c02f2cf706b66dcc454f6
fleish80 <Fleish.D@gmail.com>
2023-05-25 14:25:25 +0300

    angular signals - state management

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,27 +1,21 @@
-import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { JokeFacadeStoreService } from './joke-facade-store.service';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, JsonPipe, NgIf],
+  imports: [NgIf],
   selector: 'df-root',
   template: `
-
   <p *ngIf="this.loading(); else jokeTemplate">Loading...</p>
   <ng-template #jokeTemplate>
     <p *ngIf="!this.error()">{{this.joke()}}</p>
-    <p *ngIf="this.error()">{{this.error()?.message}}</p>
+    <p *ngIf="this.error()">{{this.error()!.message}}</p>
   </ng-template>
-
   <button (click)="loadAnotherJoke()">Load Another Joke</button>
-
   `,
-  styleUrls: [],
 })
 export class AppComponent {
-  title = 'main';
 
   #jokeFacadeStoreService = inject(JokeFacadeStoreService);
   joke = this.#jokeFacadeStoreService.joke;
--- apps/main/src/app/joke-api.util.ts
+++ apps/main/src/app/joke-api.util.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export async function getRandomJoke(): Promise<string> {
-  const response =  await axios.get<{value: string}>('https://api.chucknorris.io/jokes/random');
+  const response = await axios.get<{ value: string }>(
+    'https://api.chucknorris.io/jokes/random'
+  );
   return response.data.value;
 }
